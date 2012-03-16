4b6c09c7bcd17e5077d22e227af1f1114acc70d6
John Bintz <john@coswellproductions.com>
2012-03-16 11:15:43 -0400

    so that almost preps the project for being merged back into the flowerbox gem proper...

--- lib/flowerbox/delivery.rb
+++ lib/flowerbox/delivery.rb
@@ -1,11 +1,8 @@
 module Flowerbox
   module Delivery
     autoload :Server, 'flowerbox/delivery/server'
-    autoload :TemplateRenderer, 'flowerbox/delivery/template_renderer'
     autoload :SprocketsHandler, 'flowerbox/delivery/sprockets_handler'
     autoload :UniqueAssetList, 'flowerbox/delivery/unique_asset_list'
-
-    autoload :Tilt, 'flowerbox/delivery/tilt'
   end
 end
 
